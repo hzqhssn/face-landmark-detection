d0c4674e7004def6d2754b2248a6b53e687ddf0f
Haziq <hzq.hassan@gmail.com>
2020-11-14 20:12:21 -0600

    adding triangles

--- src/utilities.js
+++ src/utilities.js
@@ -1,6 +1,23 @@
+import { complex } from "@tensorflow/tfjs";
+
 // Triangulation metrics
 export const TRIANGULATION = [];
 // Draw triangle
+const drawPath = (ctx, points, closePath) =>{
+    const region = new Path2D();
+    region.moveTo(points[0][0], points[0][1]);
+    for(let i = 1 ; i< points.length; i++){
+        const point = points[i];
+        region.lineTo(point[0], point[1]);
+    }
+
+    if(closePath){
+        region.closePath();
+    }
+    ctx.strokeStyle = "pink";
+    ctx.stroke(region);
+}
+
 // Draw the points
 
 export const drawMesh = (predictions, ctx) => {
